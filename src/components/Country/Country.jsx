e1fe65d9db528aab7269affdc3eddc0d259d4a44
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-22 23:21:04 +0600

    card design done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -4,7 +4,15 @@ const Country = ({ country }) => {
     console.log(country)
     return (
         <div>
-            <h3>Name: {country.name.common}</h3>
+            <div className='countryCard'>
+                <img className='.countryCard__flag' src={country?.flags?.png} alt="" />
+                <div className='countryCard__content'>
+                    <h2 style={{ fontWeight: 'bold' }}>{country?.name?.common}</h2>
+                    <p>Population: {country?.population?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    <p>Region: {country?.region}</p>
+                    <p>Capital: {country?.capital}</p>
+                </div>
+            </div>
         </div>
     );
 };
