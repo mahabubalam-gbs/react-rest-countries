60bab53e349c4428dd8b9bad5a763fd095da58ae
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-26 13:00:57 +0600

    Button added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const Country = ({ country }) => {
-    console.log(country)
+    const handleVisited = () => {
+        console.log('clicked')
+    }
     return (
         <div className='countryCard'>
             <img style={{ height: '200px', width: '100%' }} src={country?.flags?.png
             } alt="" />
             <div style={{ padding: '20px 24px' }}>
-                <h4 style={{ fontWeight: 'bold', margin: '0' }}>{country?.name?.official}</h4>
+                <h2 style={{ fontWeight: 'bold' }}>{country?.name?.common}</h2>
+                <h4 style={{ margin: '0' }}>{country?.name?.official}</h4>
                 <p>Population: {country?.population?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
                 <p>Region: {country?.region}</p>
                 <p>Capital: {country?.capital}</p>
+                <button onClick={handleVisited} className='button'>Not Visited</button>
             </div>
         </div>
     );
